32eaa4c5b72b070c15027a150bceb0f8e98953e9
z9961 <laserjet1031@gmail.com>
2019-01-26 22:37:03 +0800

    禁用hostname检查

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,5 @@
-// module.exports = {
-//     devServer: {
-//         proxy: {
-//             '/api': {
-//                 target: '127.0.0.1:8888',
-//                 ws: true,
-//                 changeOrigin: true,
-//                 pathRewrite: {
-//                     // 替换target中的请求地址，请求http://aaa/xxx这个地址的时候直接写成/api/xxx即可
-//                     '^/api': ''
-//                 }
-//             }
-//         }
-//     }
-// }
+module.exports = {
+    devServer: {
+        disableHostCheck: true
+    }
+}
