6750b3ed837f312680b0ff05b1c7743094dd2a4a
z9961 <laserjet1031@gmail.com>
2019-03-04 17:12:17 +0800

    修改路由

--- src/config/axios.js
+++ src/config/axios.js
@@ -7,7 +7,6 @@ import router from './router'
 // http request 拦截器
 axios.interceptors.request.use(
     config => {
-        console.log("axios interceptors:"+store.state.token)
         if (store.state.token) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
             config.headers.Authorization = `${store.state.token}`;
         }
