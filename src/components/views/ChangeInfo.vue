1aee0d221289c1c3deee7a0461d5e5e3b44c239d
z9961 <laserjet1031@gmail.com>
2019-03-05 19:36:11 +0800

    update信息维护页

--- src/components/views/ChangeInfo.vue
+++ src/components/views/ChangeInfo.vue
@@ -7,52 +7,25 @@
         </div>
         <div class="container">
             <div class="form-box">
+                <el-alert
+                        title="不需要修改的信息请留空"
+                        type="info"
+                        center
+                        show-icon>
+                </el-alert>
+                <br>
                 <el-form ref="form" :model="form" label-width="80px">
-                    <el-form-item label="表单名称">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="邮箱">
+                        <el-input v-model="form.email"></el-input>
                     </el-form-item>
-                    <el-form-item label="选择器">
-                        <el-select v-model="form.region" placeholder="请选择">
-                            <el-option key="bbk" label="步步高" value="bbk"></el-option>
-                            <el-option key="xtc" label="小天才" value="xtc"></el-option>
-                            <el-option key="imoo" label="imoo" value="imoo"></el-option>
-                        </el-select>
+                    <el-form-item label="新密码">
+                        <el-input v-model="form.oldpassword"></el-input>
                     </el-form-item>
-                    <el-form-item label="日期时间">
-                        <el-col :span="11">
-                            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-                        </el-col>
-                        <el-col class="line" :span="2">-</el-col>
-                        <el-col :span="11">
-                            <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                        </el-col>
-                    </el-form-item>
-                    <el-form-item label="城市级联">
-                        <el-cascader :options="options" v-model="form.options"></el-cascader>
-                    </el-form-item>
-                    <el-form-item label="选择开关">
-                        <el-switch v-model="form.delivery"></el-switch>
-                    </el-form-item>
-                    <el-form-item label="多选框">
-                        <el-checkbox-group v-model="form.type">
-                            <el-checkbox label="步步高" name="type"></el-checkbox>
-                            <el-checkbox label="小天才" name="type"></el-checkbox>
-                            <el-checkbox label="imoo" name="type"></el-checkbox>
-                        </el-checkbox-group>
-                    </el-form-item>
-                    <el-form-item label="单选框">
-                        <el-radio-group v-model="form.resource">
-                            <el-radio label="步步高"></el-radio>
-                            <el-radio label="小天才"></el-radio>
-                            <el-radio label="imoo"></el-radio>
-                        </el-radio-group>
-                    </el-form-item>
-                    <el-form-item label="文本框">
-                        <el-input type="textarea" rows="5" v-model="form.desc"></el-input>
+                    <el-form-item label="新密码">
+                        <el-input v-model="form.newpassword"></el-input>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary" @click="onSubmit">表单提交</el-button>
-                        <el-button>取消</el-button>
+                        <el-button type="primary" @click="SubmitInfo">确认修改</el-button>
                     </el-form-item>
                 </el-form>
             </div>
@@ -64,76 +37,40 @@
 <script>
     export default {
         name: 'baseform',
-        data: function(){
+        data: function () {
             return {
-                options:[
-                    {
-                        value: 'guangdong',
-                        label: '广东省',
-                        children: [
-                            {
-                                value: 'guangzhou',
-                                label: '广州市',
-                                children: [
-                                    {
-                                        value: 'tianhe',
-                                        label: '天河区'
-                                    },
-                                    {
-                                        value: 'haizhu',
-                                        label: '海珠区'
-                                    }
-                                ]
-                            },
-                            {
-                                value: 'dongguan',
-                                label: '东莞市',
-                                children: [
-                                    {
-                                        value: 'changan',
-                                        label: '长安镇'
-                                    },
-                                    {
-                                        value: 'humen',
-                                        label: '虎门镇'
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        value: 'hunan',
-                        label: '湖南省',
-                        children: [
-                            {
-                                value: 'changsha',
-                                label: '长沙市',
-                                children: [
-                                    {
-                                        value: 'yuelu',
-                                        label: '岳麓区'
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ],
                 form: {
-                    name: '',
-                    region: '',
-                    date1: '',
-                    date2: '',
-                    delivery: true,
-                    type: ['步步高'],
-                    resource: '小天才',
-                    desc: '',
-                    options: []
+                    email: '',
+                    oldpassword: '',
+                    newpassword: '',
                 }
             }
         },
+        created() {
+            this.getInfo();
+        },
         methods: {
-            onSubmit() {
-                this.$message.success('提交成功！');
+            getInfo() {
+                this.$axios({
+                    method: 'GET',
+                    url: '/users/getInfo',
+                }).then(response => {
+                    var resdata = response.data;
+                    this.form.email = resdata.email;
+                });
+            },
+            SubmitInfo() {
+                this.$axios({
+                    method: 'POST',
+                    url: '/users/changeInfo',
+                }).then(response => {
+                    var resdata = response.data;
+                    if (resdata.state === "200") {
+                        this.$message.success('修改成功!');
+                    } else {
+                        this.$message.error('修改失败!请检查密码');
+                    }
+                });
             }
         }
     }
