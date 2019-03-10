fba31e9af44e0689a209e6c53644ad74aa352595
z9961 <laserjet1031@gmail.com>
2019-03-10 21:55:30 +0800

    登录验证码

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -2,33 +2,50 @@
     <div class="login-wrap">
         <div class="ms-login">
             <div class="ms-title">后台管理系统</div>
-
-
             <el-tabs type="border-card">
                 <el-tab-pane>
                     <span slot="label"><i class="el-icon-date"></i> 验证码登录</span>
-                    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content">
-                        <el-form-item prop="username">
-                            <el-input v-model="ruleForm.username" placeholder="username">
+                    <el-form :model="ruleFormCode" :rules="rulesCode" ref="ruleFormCode" label-width="0px"
+                             class="ms-content">
+                        <el-form-item prop="phone">
+                            <el-input v-model="ruleFormCode.phone" placeholder="手机号">
                                 <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
                             </el-input>
                         </el-form-item>
-                        <el-form-item prop="password">
-                            <el-input type="password" placeholder="password" v-model="ruleForm.password">
+                        <el-form-item prop="code">
+                            <el-input type="password" placeholder="验证码" v-model="ruleFormCode.code">
                                 <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
                             </el-input>
                         </el-form-item>
+                        <el-row>
+                            <el-col :span="16">
+                                <el-input v-model="ruleForm.validatecode"
+                                          v-bind:disabled="phoneinputdisabled"
+                                          placeholder="输入验证码">
+                                    <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+                                </el-input>
+                            </el-col>
+                            <el-col :span="5">
+                                <el-button
+                                        type="primary"
+                                        round
+                                        @click="getPhoneValidateCode"
+                                        v-bind:disabled="changeCodeDisabled"
+                                        v-bind:style="{opacity:changeCodeOpacity}">{{codemsg}}
+                                </el-button>
+                            </el-col>
+                        </el-row>
                         <div class="login-btn">
-                            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+                            <el-button type="primary" @click="submitForm('ruleFormCode')">登录</el-button>
                         </div>
-                        <p class="login-tips">Tips : 用户名1，密码2。</p>
+                        <p class="login-tips">Tips : 用户名1，密码1。</p>
                     </el-form>
                 </el-tab-pane>
                 <el-tab-pane>
                     <span slot="label"><i class="el-icon-date"></i> 密码登录</span>
                     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content">
-                        <el-form-item prop="username">
-                            <el-input v-model="ruleForm.username" placeholder="username">
+                        <el-form-item prop="phone">
+                            <el-input v-model="ruleForm.phone" placeholder="phone">
                                 <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
                             </el-input>
                         </el-form-item>
@@ -40,7 +57,7 @@
                         <div class="login-btn">
                             <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
                         </div>
-                        <p class="login-tips">Tips : 用户名1，密码2。</p>
+                        <p class="login-tips">Tips : 用户名1，密码1。</p>
                     </el-form>
                 </el-tab-pane>
                 <el-tab-pane>
@@ -62,23 +79,106 @@
             return {
                 msg: '',
                 ruleForm: {
-                    username: '',
+                    phone: '',
                     password: ''
                 },
                 rules: {
-                    username: [
-                        {required: true, message: '请输入用户名', trigger: 'blur'}
+                    phone: [
+                        {required: true, message: '请输入手机号', trigger: 'blur'}
                     ],
                     password: [
-                        {required: true, message: '请输入密码', trigger: 'blur'}
+                        {required: true, message: '请输入验证码', trigger: 'blur'}
                     ]
-                }
+                },
+                ruleFormCode: {
+                    phone: '',
+                    code: ''
+                },
+                rulesCode: {
+                    phone: [
+                        {required: true, message: '请输入手机号', trigger: 'blur'}
+                    ],
+                    code: [
+                        {required: true, message: '请输入验证码', trigger: 'blur'}
+                    ]
+                },
+                phoneinputdisabled: false,
+                changeCodeDisabled: false,
+                changeCodeOpacity: "1",
+                codemsg: "获取验证码",
             }
         },
         methods: {
             //新用户注册
             toRegiste() {
                 this.$router.push('/registe')
+            },
+            //获取验证码
+            getPhoneValidateCode() {
+                this.phoneinputdisabled = "true";
+                this.$axios({
+                    method: 'POST',
+                    url: '/users/getLoginValidatecode',
+                    data: {
+                        userphone: this.ruleFormCode.userphone,
+                    }
+                }).then(response => {
+                    this.changeGetCodeButtonStyle();
+                    var resdata = response.data;
+                    if (resdata.state === "200") {
+                        this.promot = "请注意接收验证码";
+                        //进行按钮倒计时
+                    } else {
+                        alert(resdata.msg);
+                    }
+                })
+            },
+            //改变获取验证码按钮样式
+            changeGetCodeButtonStyle() {
+                this.changeCodeDisabled = true;
+                setTimeout(function () {
+                    this.changeCodeOpacity = "0.6";
+                }, 1000);
+                let time = 60;
+                let set = setInterval(function () {
+                    this.codemsg = "(" + --time + ")" + "秒后获取";
+                }, 1000);
+                setTimeout(function () {
+                    this.codemsg = "重新获取验证码";
+                    this.changeCodeOpacity = "1";
+                    this.changeCodeDisabled = false;
+                    clearInterval(set);
+                }, 60000);
+            },
+            //验证码登录
+            submitCodeForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.$axios({
+                            method: 'POST',
+                            url: '/users/loginByCode',
+                            data: {
+                                phone: this.ruleFormCode.phone,
+                                code: this.ruleFormCode.code
+                            }
+                        }).then(response => {
+                            var resdata = response.data;
+                            var jsonuser = eval('(' + resdata.user + ')');
+                            if (resdata.state === "200") {
+                                this.$store.commit(types.LOGIN, {token: resdata.token, user: jsonuser});
+                                let redirect = decodeURIComponent(this.$route.query.redirect || '/');
+                                this.$router.push({
+                                    path: redirect
+                                });
+                            } else {
+                                alert("手机号或验证码错误");
+                            }
+                        })
+                    } else {
+                        alert("请输入用户名和密码");
+                        return false;
+                    }
+                });
             },
             //提交登录
             submitForm(formName) {
@@ -88,7 +188,7 @@
                             method: 'POST',
                             url: '/users/login',
                             data: {
-                                username: this.ruleForm.username,
+                                phone: this.ruleForm.phone,
                                 password: this.ruleForm.password
                             }
                         }).then(response => {
@@ -101,7 +201,7 @@
                                     path: redirect
                                 });
                             } else {
-                                alert("用户名或密码错误");
+                                alert("手机号或密码错误");
                             }
                         })
                     } else {
@@ -138,7 +238,7 @@
         position: absolute;
         left: 50%;
         top: 50%;
-        /*width: 420px;*/
+        width: 410px;
         margin: -190px 0 0 -175px;
         border-radius: 5px;
         background: rgba(255, 255, 255, 0.3);
@@ -151,6 +251,8 @@
 
     .login-btn {
         text-align: center;
+        margin-top: 35px;
+        /*height: 10px;*/
     }
 
     .login-btn button {
