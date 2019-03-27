069d1476f2d1696fe84c3f203000e1491bba1c6e
z9961 <449014690@qq.com>
2019-03-27 22:48:43 +0800

    获取请假的课程

--- src/components/views/student/MyVacate.vue
+++ src/components/views/student/MyVacate.vue
@@ -2,8 +2,8 @@
     <div class="app">
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
 
-            <el-form-item label="请假类型" prop="region">
-                <el-select v-model="ruleForm.region" placeholder="请选择请假类型">
+            <el-form-item label="请假类型" required>
+                <el-select v-model="ruleForm.vtype" placeholder="请选择请假类型">
                     <el-option label="事假" value="shanghai"></el-option>
                     <el-option label="病假" value="beijing"></el-option>
                 </el-select>
@@ -21,8 +21,23 @@
                 </el-date-picker>
             </el-form-item>
 
-            <el-form-item label="请假原因" prop="name" class="reason">
-                <el-input v-model="ruleForm.reason"
+            <el-form-item label="请假课程" required>
+                <el-select
+                        v-model="ruleForm.vcourse"
+                        filterable
+                        placeholder="请选择要请假的课程"
+                        no-data-text="您还没有课程">
+                    <el-option
+                            v-for="item in myCourse"
+                            :key="item.cname"
+                            :label="item.cname"
+                            :value="item.cname">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+
+            <el-form-item label="请假原因" required>
+                <el-input v-model="ruleForm.vreason"
                           type="textarea"
                           autosize>
                 </el-input>
@@ -41,6 +56,7 @@
         name: "MyVacate",
         data() {
             return {
+                //时间选择器快捷选项
                 pickerOptions: {
                     shortcuts: [{
                         text: '最近一天',
@@ -69,11 +85,10 @@
                     }]
                 },
                 ruleForm: {
-                    reason: '',
-                    region: '',
+                    vreason: '',
+                    vcourse: '',
                     vdatetime: '',
-                    delivery: false,
-                    type: [],
+                    vtype: '',
                 },
                 rules: {
                     vdatetime: [
@@ -82,38 +97,47 @@
                     type: [
                         {type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change'}
                     ],
-                }
+                },
+                //已加入的班级
+                myCourse: '',
             };
         },
+        created() {
+            this.getJoinedClass();
+        },
         methods: {
+            getJoinedClass() {
+                this.$axios({
+                    method: 'POST',
+                    url: '/course/getMyCourse',
+                    data: {}
+                }).then(response => {
+                    var resdata = response.data;
+                    var jsondata = eval('(' + resdata.data + ')');
+                    this.myCourse = jsondata;
+                })
+            },
             submitForm(formName) {
                 this.$refs[formName].validate((valid) => {
                     if (valid) {
                         this.$axios({
                             method: 'POST',
-                            url: '/vacate/appleVacate',
+                            url: '/vacate/createVacate',
                             data: {
-                                phone: this.ruleForm.phone,
-                                password: this.ruleForm.password
+                                vtype: this.ruleForm.vtype,
+                                vreason: this.ruleForm.vreason,
+                                vcourse: this.ruleForm.vcourse,
                             }
                         }).then(response => {
                             var resdata = response.data;
-                            var jsonuser = eval('(' + resdata.user + ')');
-                            if (resdata.state === "200") {
-                                this.$store.commit(types.LOGIN, {token: resdata.token, user: jsonuser});
-                                let redirect = decodeURIComponent(this.$route.query.redirect || '/');
-                                this.$router.push({
-                                    path: redirect
-                                });
-                            } else {
-                                alert("手机号或密码错误");
-                            }
+                            this.$alert(resdata.msg, '操作结果', {
+                                confirmButtonText: '确定',
+                            });
                         })
                     } else {
                         this.$alert("请检查信息完整性", '操作结果', {
                             confirmButtonText: '确定',
                         });
-
                         return false;
                     }
                 });
@@ -129,7 +153,8 @@
     .app {
         background-color: white
     }
-    .reason{
+
+    .reason {
         width: 50%;
     }
 </style>
