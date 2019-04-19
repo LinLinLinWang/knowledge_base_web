82ab0c425247f0eac18e509f22621fffb5d5a9e1
z9961 <449014690@qq.com>
2019-04-19 11:32:01 +0800

    fix 函数渲染

--- src/components/common/Sidebar/Item.vue
+++ src/components/common/Sidebar/Item.vue
@@ -16,27 +16,15 @@
             const {icon, title} = context.props;
             const vnodes = [];
 
-            // console.log(icon);
-
             if (icon) {
-                let si =
-            <
-                svg - icon
+                vnodes.push( < svg - icon
                 icon - class = {icon}
-                />;
-                vnodes.push(si);
+                />)
             }
 
             if (title) {
                 vnodes.push(<span slot='title'>{(title)}</span>)
             }
-
-            // console.log("vnodes:");
-            console.log( < svg - icon
-            icon - class = {icon}
-            />);
-            // console.log("end");
-
             return vnodes
         }
     }
