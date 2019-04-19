bcbf595015af965aaa38cd91af091dc3c0cd43f0
LinLinLinWang <1339567630@qq.com>
2019-04-19 12:50:07 +0800

    修改

--- src/config/router/teacher.js
+++ src/config/router/teacher.js
@@ -8,14 +8,37 @@ const teacherRouter = {
     },
     children: [
         {
-            path: '/Dashboard2',
+            path: '/createClass',
             component: resolve => require(['../../components/views/Dashboard.vue'], resolve),
             meta: {
-                title: 'example role 1',
+                title: '创建班级',
                 icon: '班级管理'
             }
         },
-
+        {
+            path: '/auditClass',
+            component: resolve => require(['../../components/views/Dashboard.vue'], resolve),
+            meta: {
+                title: '修改班级',
+                icon: '班级管理'
+            }
+        },
+        {
+            path: '/createCourse',
+            component: resolve => require(['../../components/views/Dashboard.vue'], resolve),
+            meta: {
+                title: '创建课程',
+                icon: '班级管理'
+            }
+        },
+        {
+            path: '/auditCourse',
+            component: resolve => require(['../../components/views/Dashboard.vue'], resolve),
+            meta: {
+                title: '修改课程',
+                icon: '班级管理'
+            }
+        },
     ]
 };
 
