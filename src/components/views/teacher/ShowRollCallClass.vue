c1ece2d789d07d482a27dfb3f026e65d14a92596
LinLinLinWang <1339567630@qq.com>
2019-04-27 22:32:28 +0800

    天气

--- src/components/views/teacher/ShowRollCallClass.vue
+++ src/components/views/teacher/ShowRollCallClass.vue
@@ -1,33 +1,83 @@
 <template>
     <div>
-        <el-row :gutter="16" >
+        <el-row :gutter="16">
             <el-row :gutter="16">
-                  天气 以及 周几
+                <el-card :body-style="{ padding: '5px' }">
+                    <el-row style="padding: 14px;">
+
+                        <el-col :span="6" :offset="6">
+                            <svg-icon icon-class="天气"/>
+                            <span>天气</span>
+                        </el-col>
+                        <el-col :span="6">
+                            <svg-icon icon-class="周"/>
+                            <span>周几</span>
+                        </el-col>
+                        <el-col :span="6">
+                            <svg-icon icon-class="日期"/>
+                            <span>日期</span>
+                        </el-col>
+
+                    </el-row>
+                </el-card>
             </el-row>
             <el-row :gutter="16">
                 <el-col :span="16">
-                    显示班级
+                    <el-row :gutter="24">
+                        <el-col :span="8" :offset="8">
+                            请选择课程点名
+                        </el-col>
+                        <el-col :span="8">
+
+
+                        </el-col>
+                    </el-row>
+                    <el-card :body-style="{ padding: '5px' }">
+                        <el-col :span="24">
+                            <el-table
+                                    :data="tableData"
+                                    style="width: 100%">
+                                <el-table-column
+                                        prop="date"
+                                        label="日期"
+                                        width="180">
+                                </el-table-column>
+                                <el-table-column
+                                        prop="name"
+                                        label="姓名"
+                                        width="180">
+                                </el-table-column>
+                                <el-table-column
+                                        prop="address"
+                                        label="地址">
+                                </el-table-column>
+                            </el-table>
+                        </el-col>
+                    </el-card>
                 </el-col>
-                <el-col :span="8">
-                    <el-table
-                            :data="tableData"
-                            style="width: 100%">
-                        <el-table-column
-                                prop="date"
-                                label="日期"
-                                width="180">
-                        </el-table-column>
-                        <el-table-column
-                                prop="name"
-                                label="姓名"
-                                width="180">
-                        </el-table-column>
-                        <el-table-column
-                                prop="address"
-                                label="地址">
-                        </el-table-column>
-                    </el-table>
-                </el-col>
+                <el-card :body-style="{ padding: '5px' }">
+                    <el-col :span="24">
+                        <el-table
+                                :data="tableData"
+                                style="width: 100%">
+                            <el-table-column
+                                    prop="date"
+                                    label="日期"
+                                    width="180">
+                            </el-table-column>
+                            <el-table-column
+                                    prop="name"
+                                    label="姓名"
+                                    width="180">
+                            </el-table-column>
+                            <el-table-column
+                                    prop="address"
+                                    label="地址">
+                            </el-table-column>
+                        </el-table>
+                    </el-col>
+                </el-card>
+
 
             </el-row>
         </el-row>
@@ -40,7 +90,9 @@
         name: "ShowRollClallClass",
         data() {
             return {
+                loginaddress: '',
                 value: new Date(),
+
                 tableData: [{
                     date: '2016-05-02',
                     name: '王小虎',
@@ -67,6 +119,29 @@
                     address: '上海市普陀区金沙江路 1516 弄'
                 }]
             }
+        },
+        created() {
+
+            this.getWeather();
+
+        },
+        methods: {
+
+            getWeather() {
+                this.$axios({
+                    method: 'POST',
+                    url: '/rollcall/getWeather',
+                    data: {}
+                }).then(response => {
+
+                    var resdata = response.data;
+                    var jsondata = eval('(' + resdata.data + ')');
+                    console.log(jsondata)
+                })
+
+
+            }
+
         }
     }
 </script>
@@ -77,6 +152,7 @@
         margin-bottom: 20px;
 
     }
+
     .el-col {
         border-radius: 4px;
     }
